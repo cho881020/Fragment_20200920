b43476d81ff646ca0f3a5da30fbbd17555f75405
조경진 <kj_cho@idealidea.co.kr>
2020-09-26 10:07:10 +0900

    프래그먼트상에서의 이벤트 처리  => onActivityCreated 함수 활용

--- app/src/main/java/kr/co/tjoeun/fragment_20200920/fragments/FirstFragment.kt
+++ app/src/main/java/kr/co/tjoeun/fragment_20200920/fragments/FirstFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_first.*
 import kr.co.tjoeun.fragment_20200920.R
 
 class FirstFragment : Fragment() {
@@ -16,5 +17,17 @@ class FirstFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        changeTextBtn.setOnClickListener {
+
+            firstTxt.text = "버튼이 눌림"
+
+        }
+
+    }
+
 
 }
